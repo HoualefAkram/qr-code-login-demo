b16db0f7b6158326dd8c11c08b1c675311bb4469
HoualefAkram <akram1232004@gmail.com>
2026-03-09 02:25:13 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, HTTPException
+from datetime import datetime, timedelta
+
+
+app = FastAPI()
+
+uuids = dict()
+
+
+# Helper Func
+def retrieve_uuid(uuid: str):
+    result = uuids.get(uuid)
+
+    if not result:
+        return None
+
+    if datetime.now() > result["expires_at"]:
+        uuids.pop(uuid)
+        return None
+
+    return result
+
+
+# Sent by the TV after generating the QR Code
+@app.put("/login")
+def save_uuid(uuid: str):
+    result = retrieve_uuid(uuid)
+    if result:
+        return result
+    time_to_add = timedelta(minutes=2)
+    response = {
+        "state": "pending",
+        "expires_at": datetime.now() + time_to_add,
+    }
+    uuids[uuid] = response
+    return response
+
+
+# Sent by the mobile after logging in
+@app.patch("/login")
+def save_token(uuid: str, token: str):
+    if not retrieve_uuid(uuid):
+        raise HTTPException(status_code=404, detail="Login info not found")
+    uuids[uuid]["state"] = "accepted"
+    uuids[uuid]["token"] = token
+
+
+# Fetched by the TV to login in
+@app.get("/login")
+def get_token(uuid: str):
+    result = retrieve_uuid(uuid)
+    if not result:
+        raise HTTPException(status_code=404, detail="Code expired or not found")
+    return result
